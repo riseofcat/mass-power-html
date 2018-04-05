92c7769fc7bc14ebea7f3d7e0939e2683412575a
d.avdeev <avdeev@tutu.ru>
2018-04-06 01:55:02 +0300

    shader

--- src/main/kotlin/masspower/MassPower.kt
+++ src/main/kotlin/masspower/MassPower.kt
@@ -28,7 +28,7 @@ class FixedWidth(val width:Float,val minHeight:Float,val maxHeight:Float):View()
 }
 data class Attr(val locationName:String,val numElements:Int)
 data class IterAttr(val attr:Attr,val location:Int,val offset:Int)
-class MassPower(val view:View = FixedWidth(1000f,1000f,1000f)) {//todo 1500 width   //todo высчитывать исходя из радиуса обзора
+class MassPower(val view:View = FixedWidth(1000f,1000f,1000f)) {//todo 1500 width
   val View.gameWidth:Float get() = getWidth(window.innerWidth/window.innerHeight.toFloat())*gameScale.toFloat()
   val View.gameHeight:Float get() = getHeight(window.innerWidth/window.innerHeight.toFloat())*gameScale.toFloat()
   val View.windowWidth get() = window.innerWidth.min(window.innerHeight*gameWidth/gameHeight)
@@ -48,89 +48,8 @@ class MassPower(val view:View = FixedWidth(1000f,1000f,1000f)) {//todo 1500 widt
   val cameraGamePos by CacheByRenderCalls(XY()) {model.myCar?.pos?.copy()}
   val html = HTMLElements()
   val gl get() = html.webgl
-//language=GLSL
-  val vertex = """
-//Если атрибут в шейдере не используется, то при компиляции он будет вырезан, и могут возникнуть ошибки "enableVertexAttribArray: index out of range"
-attribute float a_center_x;//игровые координаты центра круга
-attribute float a_center_y;
-attribute float a_angle;
-attribute float a_game_radius;//Радиус точки от центра в игровых координатах.
-
-attribute float a_relative_radius;//относительный радиус от [0 до 1] внутри круга и от (1 до inf) вне круга
-
-uniform float u_game_width;
-uniform float u_game_height;
-uniform float u_game_camera_x;
-uniform float u_game_camera_y;
-
-varying lowp vec2 v_textCoord;
-varying lowp float v_distance;//расстояние до круга относительно a_relative_radius. Если 0 то - в круге , если > 0 то точка на растоянии a_relative_radius * v_distance от края круга
-
-void main(void) {
-  v_distance = max(a_relative_radius - 1.0, 0.0);
-  //сейчас из png вырезается элипс, а ещё можно попробовать натягивать прямоугольник, чтобы попадали уголки png
-  v_textCoord = vec2(0.5, 0.5) + vec2(cos(a_angle), sin(a_angle)) * 0.5 * min(a_relative_radius, 1.0);
-  mat2 screenScale = mat2(2.0/u_game_width,       0.0,
-                                0.0,       2.0/u_game_height);
-  vec2 gamePos = /*a_center_pos*/vec2(a_center_x, a_center_y) + vec2(cos(a_angle)*a_game_radius, sin(a_angle)*a_game_radius);
-  gl_Position = vec4(screenScale*(gamePos - vec2(u_game_camera_x, u_game_camera_y)), 1.0, 1.0);
-  }
-"""
-  val textureShader:ShaderFull = ShaderFull(
-    ShaderVertex(vertex,listOf(/*Attr("a_center_pos",2),*/ Attr("a_center_x",1),Attr("a_center_y",1),Attr("a_angle",1),Attr("a_game_radius",1),Attr("a_relative_radius",1))),
-//language=GLSL
-"""
-precision mediump float;//todo lowp
-uniform sampler2D u_sampler;
-varying lowp vec2 v_textCoord;
-varying lowp float v_distance;//todo разобраться с invariant
-void main(void) {
-  gl_FragColor = texture2D(u_sampler, v_textCoord);
-  gl_FragColor.a = gl_FragColor.a / pow(1.0 + v_distance, 6.0);//todo потестировать performance pow() vs деление много раз
-}
-""")
-
-  val foodShader:ShaderFull = ShaderFull(
-    ShaderVertex(
-//language=GLSL
-"""
-//Если атрибут в шейдере не используется, то при компиляции он будет вырезан, и могут возникнуть ошибки "enableVertexAttribArray: index out of range"
-attribute float a_center_x;//игровые координаты центра круга
-attribute float a_center_y;
-attribute float a_angle;
-attribute float a_game_radius;
-
-attribute lowp vec4 a_color;
-
-uniform mediump float time;
-uniform float u_game_width;
-uniform float u_game_height;
-uniform float u_game_camera_x;
-uniform float u_game_camera_y;
-
-varying lowp vec4 v_color;
-
-void main(void) {
-  float posDiff = a_center_x + a_center_y;
-  v_color = a_color + vec4(cos(1.0*posDiff + a_angle + time*1.5), cos(1.5*posDiff + a_angle + time*2.0 + radians(120.0)), cos(2.0*posDiff + a_angle + time*2.5 + radians(240.0)), 0.0)*(sign(2.0*posDiff + a_game_radius)+0.3);
-  v_color = v_color - (1.0 - sign(a_game_radius))*cos(time)*vec4(1.0,1.0,1.0,0.4);
-  //сейчас из png вырезается элипс, а ещё можно попробовать натягивать прямоугольник, чтобы попадали уголки png
-  mat2 screenScale = mat2(2.0/u_game_width,       0.0,
-                                0.0,       2.0/u_game_height);
-  vec2 gamePos = /*a_center_pos*/vec2(a_center_x, a_center_y) + vec2(cos(a_angle)*a_game_radius, sin(a_angle)*a_game_radius);
-  gl_Position = vec4(screenScale*(gamePos - vec2(u_game_camera_x, u_game_camera_y)), 1.0, 1.0);
-  }
-""",
-      listOf(/*Attr("a_center_pos",2),*/ Attr("a_center_x",1),Attr("a_center_y",1), Attr("a_angle",1), Attr("a_game_radius",1), Attr("a_color",4))
-    ),
-//language=GLSL
-"""
-precision mediump float;
-varying lowp vec4 v_color;
-void main(void) {
-  gl_FragColor = v_color;
-}
-""")
+  val textureShader:ShaderFull = ShaderFull(ShaderVertex(MASS_POWER_TEXTURE_VERTEX,listOf(/*Attr("a_center_pos",2),*/ Attr("a_center_x",1),Attr("a_center_y",1),Attr("a_angle",1),Attr("a_game_radius",1),Attr("a_relative_radius",1))),MASS_POWER_TEXTURE_FRAG)
+  val foodShader:ShaderFull = ShaderFull(ShaderVertex(MASS_POWER_FOOD_VERTEX,listOf(/*Attr("a_center_pos",2),*/ Attr("a_center_x",1),Attr("a_center_y",1),Attr("a_angle",1),Attr("a_game_radius",1),Attr("a_color",4))),MASS_POWER_FOOD_FRAG)
   val backgroundShader = ShaderFull(ShaderVertex(shader_mesh_default_vert, listOf(Attr("aVertexPosition",2))), shader_background_stars_frag)
   private val imgCache:MutableMap<ImgData,ImgCache> = hashMapOf()
   var mousePos:XY = XY()
@@ -266,9 +185,10 @@ void main(void) {
     setUniformf("time", pow2in14 - lib.pillarTimeS(2*pow2in14).toFloat())//lowp от -2.0 до 2.0
     render(Mode.TRIANGLE,-1f,-1f,-1f,1f,1f,-1f,1f,1f,-1f,1f,1f,-1f)
     foodShader.activate()
+    val visibleRadius = view.gameWidth*0.75//todo умнее height тоже
     state?.foods?.forEach {
       val xy = calcRenderXY(state,XY(it.pos.x,it.pos.y),cameraGamePos)
-      if((cameraGamePos - xy).len < 1000f) {//todo высчитывать радиус обзора и применять к cars и reactive
+      if((cameraGamePos - xy).len <visibleRadius) {//todo высчитывать радиус обзора и применять к cars и reactive
         val fan = CircleData(defaultBlend){angle-> floatArrayOf(0f, 0f, 0f, 0f)}
         renderCircle10(xy.x.toFloat(), xy.y.toFloat(), it.radius*FOOD_SCALE, null,floatArrayOf(1.5f, 1.5f, 1.5f, 1f),fan)
       }
